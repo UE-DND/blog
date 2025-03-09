76e0e345c9c26c0fae38a454b0c19124bb15823a
UE-DND <wt.chen.uednd@gmail.com>
2025-03-09 21:50:25 +0800

    修复了一些问题
    修复关闭右键菜单某些功能时出现多余分割线；
    修复右键菜单悬停颜色与主题不统一；
    修复某些元素圆角曲率不统一；
    移除所有不必需的国际化支持；

--- components/CustomContextMenu.js
+++ components/CustomContextMenu.js
@@ -166,24 +166,24 @@ export default function CustomContextMenu(props) {
       style={{ top: position.y, left: position.x }}
       className={`${show ? '' : 'invisible opacity-0'} select-none transition-opacity duration-200 fixed z-50`}>
       {/* 菜单内容 */}
-      <div className='rounded-xl w-52 dark:hover:border-yellow-600 bg-white dark:bg-[#040404] dark:text-gray-200 dark:border-gray-600 p-3 border drop-shadow-lg flex-col duration-300 transition-colors'>
+      <div className='rounded-md w-52 dark:hover:border-yellow-600 bg-white dark:bg-[#040404] dark:text-gray-200 dark:border-gray-600 p-3 border drop-shadow-lg flex-col duration-300 transition-colors'>
         {/* 顶部导航按钮 */}
         <div className='flex justify-between'>
           <i
             onClick={handleBack}
-            className='hover:bg-blue-600 hover:text-white px-2 py-2 text-center w-8 rounded cursor-pointer fa-solid fa-arrow-left'></i>
+            className='hover:bg-gray-100 hover:text-gray-800 px-2 py-2 text-center w-8 rounded cursor-pointer fa-solid fa-arrow-left'></i>
           <i
             onClick={handleForward}
-            className='hover:bg-blue-600 hover:text-white px-2 py-2 text-center w-8 rounded cursor-pointer fa-solid fa-arrow-right'></i>
+            className='hover:bg-gray-100 hover:text-gray-800 px-2 py-2 text-center w-8 rounded cursor-pointer fa-solid fa-arrow-right'></i>
           <i
             onClick={handleRefresh}
-            className='hover:bg-blue-600 hover:text-white px-2 py-2 text-center w-8 rounded cursor-pointer fa-solid fa-rotate-right'></i>
+            className='hover:bg-gray-100 hover:text-gray-800 px-2 py-2 text-center w-8 rounded cursor-pointer fa-solid fa-rotate-right'></i>
           <i
             onClick={handleScrollTop}
-            className='hover:bg-blue-600 hover:text-white px-2 py-2 text-center w-8 rounded cursor-pointer fa-solid fa-arrow-up'></i>
+            className='hover:bg-gray-100 hover:text-gray-800 px-2 py-2 text-center w-8 rounded cursor-pointer fa-solid fa-arrow-up'></i>
         </div>
 
-        <hr className='my-2 border-dashed' />
+        {(CUSTOM_RIGHT_CLICK_CONTEXT_MENU_RANDOM_POST || CUSTOM_RIGHT_CLICK_CONTEXT_MENU_CATEGORY || CUSTOM_RIGHT_CLICK_CONTEXT_MENU_TAG) && <hr className='my-2 border-solid border-gray-200' />}
 
         {/* 跳转导航按钮 */}
         <div className='w-full px-2'>
@@ -191,7 +191,7 @@ export default function CustomContextMenu(props) {
             <div
               onClick={handleJumpToRandomPost}
               title={locale.MENU.WALK_AROUND}
-              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-lg duration-200 transition-all'>
+              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-md duration-200 transition-all'>
               <i className='fa-solid fa-podcast mr-2' />
               <div className='whitespace-nowrap'>{locale.MENU.WALK_AROUND}</div>
             </div>
@@ -201,7 +201,7 @@ export default function CustomContextMenu(props) {
             <Link
               href='/category'
               title={locale.MENU.CATEGORY}
-              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-lg duration-200 transition-all'>
+              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-md duration-200 transition-all'>
               <i className='fa-solid fa-square-minus mr-2' />
               <div className='whitespace-nowrap'>{locale.MENU.CATEGORY}</div>
             </Link>
@@ -211,14 +211,14 @@ export default function CustomContextMenu(props) {
             <Link
               href='/tag'
               title={locale.MENU.TAGS}
-              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-lg duration-200 transition-all'>
+              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-md duration-200 transition-all'>
               <i className='fa-solid fa-tag mr-2' />
               <div className='whitespace-nowrap'>{locale.MENU.TAGS}</div>
             </Link>
           )}
         </div>
 
-        <hr className='my-2 border-dashed' />
+        {(CUSTOM_RIGHT_CLICK_CONTEXT_MENU_RANDOM_POST || CUSTOM_RIGHT_CLICK_CONTEXT_MENU_CATEGORY || CUSTOM_RIGHT_CLICK_CONTEXT_MENU_TAG) && <hr className='my-2 border-solid border-gray-200' />}
 
         {/* 功能按钮 */}
         <div className='w-full px-2'>
@@ -226,7 +226,7 @@ export default function CustomContextMenu(props) {
             <div
               onClick={handleCopy}
               title={locale.MENU.COPY}
-              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-lg duration-200 transition-all'>
+              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-md duration-200 transition-all'>
               <i className='fa-solid fa-copy mr-2' />
               <div className='whitespace-nowrap'>{locale.MENU.COPY}</div>
             </div>
@@ -236,7 +236,7 @@ export default function CustomContextMenu(props) {
             <div
               onClick={handleCopyLink}
               title={locale.MENU.SHARE_URL}
-              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-lg duration-200 transition-all'>
+              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-md duration-200 transition-all'>
               <i className='fa-solid fa-arrow-up-right-from-square mr-2' />
               <div className='whitespace-nowrap'>{locale.MENU.SHARE_URL}</div>
             </div>
@@ -248,7 +248,7 @@ export default function CustomContextMenu(props) {
               title={
                 isDarkMode ? locale.MENU.LIGHT_MODE : locale.MENU.DARK_MODE
               }
-              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-lg duration-200 transition-all'>
+              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-md duration-200 transition-all'>
               {isDarkMode ? (
                 <i className='fa-regular fa-sun mr-2' />
               ) : (
@@ -265,7 +265,7 @@ export default function CustomContextMenu(props) {
             <div
               onClick={handleChangeTheme}
               title={locale.MENU.THEME_SWITCH}
-              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-lg duration-200 transition-all'>
+              className='w-full px-2 h-10 flex justify-start items-center flex-nowrap cursor-pointer hover:bg-blue-600 hover:text-white rounded-md duration-200 transition-all'>
               <i className='fa-solid fa-palette mr-2' />
               <div className='whitespace-nowrap'>
                 {locale.MENU.THEME_SWITCH}
